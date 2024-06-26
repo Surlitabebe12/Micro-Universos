5a1815b60cc850bf116b8de00f6536d1cd3d2983
Surlitabebe12 <62902938+Surlitabebe12@users.noreply.github.com>
2024-06-26 18:53:10 -0300

    Update increment-counter.js

--- api/increment-counter.js
+++ api/increment-counter.js
@@ -5,17 +5,18 @@ export default async (req, res) => {
     const counterPath = path.join(process.cwd(), 'counter.txt');
 
     try {
-        // Leer el contenido actual del archivo counter.txt
+        console.log('Reading counter from:', counterPath);
         let counter = parseInt(fs.readFileSync(counterPath, 'utf-8'), 10);
+        console.log('Current counter value:', counter);
 
-        // Incrementar el contador
         counter += 1;
 
-        // Escribir el nuevo valor en counter.txt
         fs.writeFileSync(counterPath, counter.toString(), 'utf-8');
+        console.log('New counter value:', counter);
 
         res.status(200).json({ counter });
     } catch (error) {
+        console.error('Error incrementing counter:', error);
         res.status(500).json({ error: 'Error incrementing counter' });
     }
 };
