fff3b82c8dc02875775f1ca2b5a15436fdd32dc3
Rodrigo <rbrea7@hotmail.com>
2024-06-29 12:51:35 -0300

    cambio index

--- Public/script.js
+++ Public/script.js
@@ -247,7 +247,7 @@ function openProductModal(productId) {
         const productModalCode = document.getElementById('product-modal-code');
 
         productModalImage.src = product.images[0];
-        productModalDescription.textContent = `<span class="small-description">Descripción General:</span><br><span class="small-description">${product.description || ''}</span>`;
+        productModalDescription.innerHTML = `<span class="small-description">Descripción General:</span><br><span class="small-description">${product.description || ''}</span>`;
 
         if (product.code) {
             productModalCode.textContent = product.code;
